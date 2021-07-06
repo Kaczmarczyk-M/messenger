fdbd97166f4dd5642bd708a145617f36179f96fd
Kaczmarczyk-M <m01kaczmarczyk@gmail.com>
2021-07-06 11:13:38 +0200

    basic wygląd, wiadomości dodają się od dołu

---
+++ script.js
@@ -0,0 +1,21 @@
+
+var tresc_diva = [];
+
+function refresh()
+{
+    var calyChat = "";
+    for(var i = 0; i < tresc_diva.length; i++)
+    {
+        calyChat += tresc_diva[i];
+    }
+    document.getElementById("chat").innerHTML = tresc_diva.join('');
+}
+
+function submit()
+{
+    tresc_diva.push('<div class="log">' + document.getElementById("message").value + '</div>');
+    tresc_diva.push('<div style="clear: both;"></div>')
+    document.getElementById("message").value = ""
+    // document.getElementById("chat").innerHTML = document.getElementById("message").value
+    refresh();
+}
